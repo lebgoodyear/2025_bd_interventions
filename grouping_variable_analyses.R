97e4945539b3d3301418e60af3917ffb271813f7
lebgoodyear <lebgoodyear@protonmail.com>
2025-09-24 16:23:35 +0100

    Add r2 calculation and conclusion

--- grouping_variable_analyses.R
+++ grouping_variable_analyses.R
@@ -5,7 +5,7 @@
 
 # Author: Luke Goodyear (lgoodyear01@qub.ac.uk)
 # Date created: Feb 2025
-# Last edited: Feb 2025
+# Last edited: Sept 2025
 
 
 # clear workspace
@@ -32,16 +32,16 @@ source(paste0(path, "../../functions_for_plotting.R"))
 
 # zero inflated beta family cannot have values equal to 1 so set 1 as nearly 1
 df$Success_brms <- df$Success
-df$Success_brms[df$Success_brms == 1] <- 0.9999999     
+df$Success_brms[df$Success_brms == 1] <- 0.9999999
 
 # increase max size to enable reloo option for large sizes in loo()
-options(future.globals.maxSize=8000* 1024^3)
+options(future.globals.maxSize = 8000 * 1024^3)
 
 # create output folder for brms outputs
 path_out_brms_gv <- paste0(path, "brms/grouping_variables/")
-ifelse(!dir.exists(file.path(path_out_brms_gv)), 
-        dir.create(file.path(path_out_brms_gv), recursive=T), 
-        FALSE)
+ifelse(!dir.exists(file.path(path_out_brms_gv)),
+       dir.create(file.path(path_out_brms_gv), recursive = TRUE),
+       FALSE)
 
 
 ################################################################################
@@ -52,19 +52,19 @@ ifelse(!dir.exists(file.path(path_out_brms_gv)),
 table(df$Efficacy.Matrix)
 
 # infection matrix
-infm <- df[df$Efficacy.Matrix == "Infection matrix",]
+infm <- df[df$Efficacy.Matrix == "Infection matrix", ]
 mean(infm$Success)
 table(infm$Intervention.category.1)
 table(infm$Specific.treatment.used.1[infm$Intervention.category.1 == "Chemical"])
 
 # prevelance matrix
-prevm <- df[df$Efficacy.Matrix == "Prevelance matrix",]
+prevm <- df[df$Efficacy.Matrix == "Prevelance matrix", ]
 mean(prevm$Success)
 table(prevm$Intervention.category.1)
 table(prevm$Specific.treatment.used.1[prevm$Intervention.category.1 == "Chemical"])
 
 # survivability matrix
-survm <- df[df$Efficacy.Matrix == "Survivability matrix",]
+survm <- df[df$Efficacy.Matrix == "Survivability matrix", ]
 mean(survm$Success)
 table(survm$Intervention.category.1)
 table(survm$Specific.treatment.used.1[survm$Intervention.category.1 == "Chemical"])
@@ -82,117 +82,129 @@ eff_violin
 ############################## PUBLICATION #####################################
 
 
-modb_pub <- brm(Success_brms | weights(Uncertainty_weights) ~ 1 + 
-                         (1 | Publication.Date.Authorship),
-                data = df[!(is.na(df$SVLMx)),],
-                family=zero_inflated_beta(),
+modb_pub <- brm(Success_brms | weights(Uncertainty_weights) ~ 1 +
+                  (1 | Publication.Date.Authorship),
+                data = df[!(is.na(df$SVLMx)), ],
+                family = zero_inflated_beta(),
                 save_pars = save_pars(all = TRUE),
-                iter=1e4,
-                cores=4,
-                chains=4)
+                iter = 1e4,
+                cores = 4,
+                chains = 4)
 summary(modb_pub)
 saveRDS(modb_pub, paste0(path_out_brms_gv, "modb_pub.rds"))
 modb_pub <- readRDS(paste0(path_out_brms_gv, "modb_pub.rds"))
 # diagnsotics
 #pp_check(modb_pub)
 #plot(modb_pub)
-loo_pub <- loo(modb_pub, moment_match=TRUE, reloo=TRUE)
+loo_pub <- loo(modb_pub, moment_match = TRUE, reloo = TRUE)
 loo_pub
 saveRDS(loo_pub, paste0(path_out_brms_gv, "loo_pub.rds"))
 loo_pub <- readRDS(paste0(path_out_brms_gv, "loo_pub.rds"))
 
 
-# EFFICACY MATRIX
-modb_effmat <- brm(Success_brms | weights(Uncertainty_weights) ~ 1 + 
-                         (1 | Efficacy.Matrix),
-                data = df[!(is.na(df$SVLMx)),],
-                family=zero_inflated_beta(),
-                save_pars = save_pars(all = TRUE),
-                control = list(adapt_delta = 0.99),
-                iter=1e5,
-                cores=4,
-                chains=4)
+########################### EFFICACY MATRIX ####################################
+
+
+modb_effmat <- brm(Success_brms | weights(Uncertainty_weights) ~ 1 +
+                     (1 | Efficacy.Matrix),
+                   data = df[!(is.na(df$SVLMx)), ],
+                   family = zero_inflated_beta(),
+                   save_pars = save_pars(all = TRUE),
+                   control = list(adapt_delta = 0.99),
+                   iter = 1e5,
+                   cores = 4,
+                   chains = 4)
 summary(modb_effmat)
 saveRDS(modb_effmat, paste0(path_out_brms_gv, "modb_effmat.rds"))
 modb_effmat <- readRDS(paste0(path_out_brms_gv, "modb_effmat.rds"))
 # diagnostics
 #pp_check(modb_effmat)
 #plot(modb_effmat)
-loo_effmat <- loo(modb_effmat, moment_match=TRUE, reloo=TRUE)
+loo_effmat <- loo(modb_effmat, moment_match = TRUE, reloo = TRUE)
 loo_effmat
 saveRDS(loo_effmat, paste0(path_out_brms_gv, "loo_effmat.rds"))
 loo_effmat <- readRDS(paste0(path_out_brms_gv, "loo_effmat.rds"))
 
 
-# GENUS
-modb_genus <- brm(Success_brms | weights(Uncertainty_weights) ~ 1 + 
-                         (1 | Genus),
-                data = df[!(is.na(df$SVLMx)),],
-                family=zero_inflated_beta(),
-                save_pars = save_pars(all = TRUE),
-                iter=1e4,
-                cores=4,
-                chains=4)
+################################# GENUS ########################################
+
+
+modb_genus <- brm(Success_brms | weights(Uncertainty_weights) ~ 1 +
+                    (1 | Genus),
+                  data = df[!(is.na(df$SVLMx)), ],
+                  family = zero_inflated_beta(),
+                  save_pars = save_pars(all = TRUE),
+                  iter = 1e4,
+                  cores = 4,
+                  chains = 4)
 summary(modb_genus)
 saveRDS(modb_genus, paste0(path_out_brms_gv, "modb_genus.rds"))
 modb_genus <- readRDS(paste0(path_out_brms_gv, "modb_genus.rds"))
 # diagnostics
 #pp_check(modb_genus)
 #plot(modb_genus)
-loo_genus <- loo(modb_genus, moment_match=TRUE, reloo=TRUE)
+loo_genus <- loo(modb_genus, moment_match = TRUE, reloo = TRUE)
 loo_genus
 saveRDS(loo_genus, paste0(path_out_brms_gv, "loo_genus.rds"))
 loo_genus <- readRDS(paste0(path_out_brms_gv, "loo_genus.rds"))
 
 
-# PUBLICATION AND EFFICACY AND GENUS
-modb_pub_effmat_genus <- brm(Success_brms | weights(Uncertainty_weights) ~ 
-                                        (1 | Publication.Date.Authorship) + 
-                                        (1 | Efficacy.Matrix)+
-                                        (1 | Genus),
-                data = df[!(is.na(df$SVLMx)),],
-                family=zero_inflated_beta(),
-                save_pars = save_pars(all = TRUE),
-                control = list(adapt_delta = 0.95),
-                iter=1e4,
-                cores=4,
-                chains=4)
+################## PUBLICATION AND EFFICACY AND GENUS ##########################
+
+
+modb_pub_effmat_genus <- brm(Success_brms | weights(Uncertainty_weights) ~
+                               (1 | Publication.Date.Authorship) +
+                                 (1 | Efficacy.Matrix) +
+                                 (1 | Genus),
+                             data = df[!(is.na(df$SVLMx)), ],
+                             family = zero_inflated_beta(),
+                             save_pars = save_pars(all = TRUE),
+                             control = list(adapt_delta = 0.95),
+                             iter = 1e4,
+                             cores = 4,
+                             chains = 4)
 summary(modb_pub_effmat_genus)
 saveRDS(modb_pub_effmat_genus, paste0(path_out_brms_gv, "modb_effmat_pub_genus.rds"))
 modb_pub_effmat_genus <- readRDS(paste0(path_out_brms_gv, "modb_effmat_pub_genus.rds"))
 # diagnostics
 #pp_check(modb_pub_effmat_genus)
 #plot(modb_pub_effmat_genus)
-loo_pub_effmat_genus <- loo(modb_pub_effmat_genus, moment_match=TRUE, reloo=TRUE)
+loo_pub_effmat_genus <- loo(modb_pub_effmat_genus, moment_match = TRUE, reloo = TRUE)
 loo_pub_effmat_genus
 saveRDS(loo_pub_effmat_genus, paste0(path_out_brms_gv, "loo_pub_effmat_genus.rds"))
 loo_pub_effmat_genus <- readRDS(paste0(path_out_brms_gv, "loo_pub_effmat_genus.rds"))
 
 
-# PUBLICATION AND EFFICACY
-modb_pub_effmat <- brm(Success_brms | weights(Uncertainty_weights) ~ 
-                                        (1 | Publication.Date.Authorship) + 
-                                        (1 | Efficacy.Matrix),
-                data = df[!(is.na(df$SVLMx)),],
-                family=zero_inflated_beta(),
-                save_pars = save_pars(all = TRUE),
-                control = list(adapt_delta = 0.95),
-                iter=1e4,
-                cores=4,
-                chains=4)
+####################### PUBLICATION AND EFFICACY ###############################
+
+
+modb_pub_effmat <- brm(Success_brms | weights(Uncertainty_weights) ~
+                         (1 | Publication.Date.Authorship) +
+                           (1 | Efficacy.Matrix),
+                       data = df[!(is.na(df$SVLMx)), ],
+                       family = zero_inflated_beta(),
+                       save_pars = save_pars(all = TRUE),
+                       control = list(adapt_delta = 0.95),
+                       iter = 1e4,
+                       cores = 4,
+                       chains = 4)
 summary(modb_pub_effmat)
 saveRDS(modb_pub_effmat, paste0(path_out_brms_gv, "modb_effmat_pub.rds"))
 modb_pub_effmat <- readRDS(paste0(path_out_brms_gv, "modb_effmat_pub.rds"))
 # diagnostics
 #pp_check(modb_pub_effmat)
 #plot(modb_pub_effmat)
-loo_pub_effmat <- loo(modb_pub_effmat, moment_match=TRUE, reloo=TRUE)
-loo_effmat_pub
+loo_pub_effmat <- loo(modb_pub_effmat, moment_match = TRUE, reloo = TRUE)
+loo_pub_effmat
 saveRDS(loo_pub_effmat, paste0(path_out_brms_gv, "loo_pub_effmat.rds"))
 loo_pub_effmat <- readRDS(paste0(path_out_brms_gv, "loo_pub_effmat.rds"))
 
 
-# compare grouping models
+################################################################################
+############################## Comparison ######################################
+
+
+# compare grouping models with loo
 loo_compare(
   loo_pub,
   loo_effmat,
@@ -200,6 +212,30 @@ loo_compare(
   loo_pub_effmat,
   loo_pub_effmat_genus
 )
+
+# compare grouping models with r2
+models <- list(modb_pub_effmat, modb_pub_effmat_genus, modb_effmat, modb_pub, modb_genus)
+names(models) <- c("pub_effmat", "pub_effmat_genus", "effmat", "pub", "genus")
+
+# find r2 distributions
+r2_comparison <- sapply(models, bayes_R2)
+
+# print results in a friendly format
+r2_results <- data.frame(
+  R2_mean = r2_comparison[1, ],
+  R2_sd = r2_comparison[2, ],
+  R2_lower = r2_comparison[3, ],
+  R2_upper = r2_comparison[4, ]
+)
+print(r2_results)
+
+
+# loo and r2 show that genus doesn't affect success, and publication and
+# efficacy matrix are probably capturing the same signal (correlated and have
+# similar explanatory power). The combined model is likely suffering from
+# overfitting because of this. Publication has more levels and is more
+# likely to explain more of the variation so use only publication for
+# random effect.
 
 
 ## end of script
